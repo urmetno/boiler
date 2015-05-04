3089d69a9f797c7b6b1d6834184343e347468d3a
urmetn <urmetn@greeny.cs.tlu.ee>
2015-05-04 19:12:20 +0300

    algus

--- Boiler.java
+++ Boiler.java
@@ -4,6 +4,7 @@ public class Boiler{
 	public Boiler(double maht, double temperatuur){
 		this.maht=maht; this.temperatuur=temperatuur;
 	}
+	//see on kommentaar
 	public void kyta(double vatid, double sekundid){
 		double dzaulid=vatid*sekundid;
 		temperatuur+=dzaulid/4.18/1000;
